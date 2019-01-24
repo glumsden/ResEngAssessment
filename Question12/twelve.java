baf6b2ac7b2371c6178b35a057c77cdfabcc9ea9
glumsden <47013767+glumsden@users.noreply.github.com>
2019-01-24 23:01:37 +0000

    Add files via upload

--- Question12/twelve.java
+++ Question12/twelve.java
@@ -2,27 +2,25 @@ import java.util.*;
 import java.util.Scanner;
 public class twelve
 {
-  // arguments are passed using the text field below this editor
   public static void main(String[] args)
   {
-    Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC")); //instantiate a Calendar object in UTC format
 	calendar.clear();
-	//calendar.set(2000, Calendar.APRIL, 26);
-	Scanner reader = new Scanner(System.in);
+	Scanner reader = new Scanner(System.in); Scanner to allow us read in form command line
 	
-	System.out.println("Enter the year: ");
-	int year = reader.nextInt();
+	System.out.println("Enter the year: "); //request year
+	int year = reader.nextInt(); //read it in int format
 	
-	System.out.println("Enter the month in format ## (eg: January = 01): ");
-	int month = reader.nextInt();
-	month--;
+	System.out.println("Enter the month in format ## (eg: January = 01): "); //request month in particular format. Could possibly add validation here
+	int month = reader.nextInt(); //read it in int format
+	month--; //minus 1 from what user entered since month starts at index 0 and not 1
 	
-	System.out.println("Enter the day: ");
-	int day = reader.nextInt();	
+	System.out.println("Enter the day: "); //request day
+	int day = reader.nextInt();	//read it in int format
 	
-	calendar.set(year, month, day);
+	calendar.set(year, month, day); //set the date in calendar
 	
-	long secondsSinceEpoch = calendar.getTimeInMillis() / 1000L;
-	System.out.println(secondsSinceEpoch);
+	long secondsSinceEpoch = calendar.getTimeInMillis() / 1000L; //instantiate long called secondsSinceEpoch with the number of milliseconds since epoch (which is what the mothod .getTimeInMillis does) and divide by 1000 to get it in seconds.
+	System.out.println(secondsSinceEpoch); //print out the results
   }
 }
