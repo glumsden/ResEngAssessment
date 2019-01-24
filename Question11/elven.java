a07a4109088a0f9f216acb0b3f6c8d9a5ecd2464
glumsden <47013767+glumsden@users.noreply.github.com>
2019-01-24 22:53:58 +0000

    Add files via upload

--- Question11/elven.java
+++ Question11/elven.java
@@ -1,25 +1,21 @@
 import java.util.*;
 public class elven
 {
-  // arguments are passed using the text field below this editor
   public static void main(String[] args)
   {
-    //String myObject = ("Hello World!");
-    //System.out.print(myObject);
     
-    String patchText = ("108835|04|Feb/06/03|R|S|O| |8|sparc;108827-30;|SUNWdtdst:1.4,REV=10.1999.12.02;SUNWdtdmn:1.4,REV=10.1999.12.02;|Obsoleted by: 108835-05 CDE 1.4: dtcm patch");
-	StringTokenizer st = new StringTokenizer(patchText, "|");
+    String patchText = ("108835|04|Feb/06/03|R|S|O| |8|sparc;108827-30;|SUNWdtdst:1.4,REV=10.1999.12.02;SUNWdtdmn:1.4,REV=10.1999.12.02;|Obsoleted by: 108835-05 CDE 1.4: dtcm patch"); //set the patch info as a String
+	StringTokenizer st = new StringTokenizer(patchText, "|"); //instantiate a StringTokenizer, pass the patch info string and delimit using the | character
 	int numTokens = st.countTokens();
 	System.out.println("The Number of Tokens is: " + numTokens);
-	String[] strArr = new String[numTokens];
+	String[] strArr = new String[numTokens]; //create an array for the tokens large enough to accommodate the number of tokens
 	int count = 0;
-    while (st.hasMoreTokens()) 
+    while (st.hasMoreTokens()) //While StringTokenizer has more tokens keep adding them to the Array
 	{
-		//System.out.println(st.nextToken());
 		strArr[count] = st.nextToken();
 		count++;
     }
-	for(int i = 0; i < strArr.length; i++)
+	for(int i = 0; i < strArr.length; i++) //Print out the tokens 
 	{
 		System.out.println(strArr[i]);
 	}
